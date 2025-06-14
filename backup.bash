1caf526ba938ea84d7b32909b01cfa2d1942f51c
ethan.cole <ethananilcole@gmail.com>
2025-06-14 12:53:06 +0100

    Backup at Sat, Jun 14, 2025 12:53:06 PM

--- backup.bash
+++ backup.bash
@@ -8,7 +8,7 @@ set -e;
 # MOD_SUBDIR_NAME="CircleOfTheSpores_db7e15fd-b2fc-b159-4bbd-1baab34d8c3a"
 # Look in "C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Projects"
 # for names of mods you have already started
-MOD_SUBDIR_NAME=""
+MOD_SUBDIR_NAME="DyyttemsSubclassPack_24ee62eb-0056-f845-c2a1-73a81f1fd768"
 
 # This is the MinGW64 path to a Steam install of the toolkit
 BG3_DATA="/c/Program Files (x86)/Steam/steamapps/common/Baldurs Gate 3/Data"
